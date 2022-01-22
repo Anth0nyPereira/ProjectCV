391db2ab4c429a117da6dff2dabffbfa26170433
Carla Sofia <carla.s.oliveira@ua.pt>
2022-01-22 22:29:03 +0000

    Explosion with light

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
+    public GameObject explosion; // drag your explosion prefab here
+    float duration = 3.0f;
+    float originalRange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +23,21 @@ public class TargetController : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Arrow")){
-
+            GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
             Destroy(this.gameObject);
+            Destroy(expl, 1); // delete the explosion after 1 seconds
+
+            /*GameObject lightGameObject = new GameObject("The Light");
+            // Add the light component
+            Light lightComp = lightGameObject.AddComponent<Light>();
+            var amplitude = Mathf.PingPong(Time.time, duration);
+            amplitude = amplitude / duration * 0.5f + 0.5f;
+            lightComp.range = lightComp.range * amplitude;
+            // Set color and position
+            lightComp.color = Color.red;
+            // Set the position (or any transform property)
+            lightGameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            Destroy(lightGameObject, 3);*/
         }
     }
 }
